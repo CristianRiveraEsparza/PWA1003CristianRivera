2e65a4cdf7636724e9662bab92f01fb1f2afefeb
CristianRiveraEsparza <cristianriveraesparza890@gmail.com>
2022-09-10 09:46:12 -0500

    ActividadClase

---
+++ src/repaso/01-tipos-datos.ts
@@ -0,0 +1,21 @@
+
+
+console.log('Hola Mundo!-pwa-1003');
+
+let nombre:string="Rivera"
+let cont:number=111;
+let acredita:boolean=false;
+
+let apaterno:number | string='juan';
+
+apaterno=344;
+
+var miVariable:string='Hola';
+
+const tem:number=345;
+
+
+/*
+    ===== Código de TypeScript =====
+*/
+
