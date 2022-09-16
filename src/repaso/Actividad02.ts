bf9b8397d94180092181c715c4f6d1c46f867379
CristianRiveraEsparza <cristianriveraesparza890@gmail.com>
2022-09-15 22:31:42 -0500

    Actualizacion del 15 jakjkjakjsk

--- src/repaso/Actividad02.ts
+++ src/repaso/Actividad02.ts
@@ -1,3 +1,16 @@
-class Formula{
-    
+class FormulaGeneral{
+    private v1:number;
+    private v2:number;
+    public respuesta;
+    public resultado(){
+        this.respuesta();
+    }
+
+    private valor(){
+        this.v1=Math.floor(Math.random()+5);
+        this.v2=Math.floor(Math.random()+5);
+    }
+    public imprimir(){
+        console.log(`La respuesta ${this.v1}+${this.v2}= ${this.respuesta}`)
+    }
 }
